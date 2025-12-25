578ee47d0cf6054894f59341e5e84ef49bc5b523
Abdallah <abdullahelawady68@gmail.com>
2025-12-25 14:06:16 +0200

    Switch to HashRouter for reliable GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { LanguageProvider } from './contexts/LanguageContext'
 import './index.css'
 import App from './App.jsx'
@@ -8,9 +8,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
-      <BrowserRouter basename="/B2-SOUQ-Dashboard">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   </StrictMode>,
 )
